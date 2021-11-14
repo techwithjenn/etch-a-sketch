6e363b08f6f32382edf64c7afddd56b898334fc9
techwithjenn <93089897+techwithjenn@users.noreply.github.com>
2021-11-14 17:01:31 -0500

    Fixed all functionality (pen color, grid size)

--- script.js
+++ script.js
@@ -5,47 +5,60 @@ function makeRows(rows, cols) {
     drawingCanvas.style.setProperty('--grid-cols', cols);
     for (c =0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
-        
-        cell.addEventListener('mouseover', function() {
-            changeColor(cell);
-        })
         drawingCanvas.appendChild(cell).className = 'grid-item';
-    };
-};
-
-function changeColor (cellunit) {
-    const blackColor = document.getElementById('black');
-    const rainbowColor = document.getElementById('rainbow');
-    const grayscaleColor = document.getElementById('grayscale');
-    if (blackColor.checked) {
-        cellunit.style.backgroundColor = 'black';
-    }
-    else if (rainbowColor.checked) {
-        cellunit.className = 'rainbow';
-    }
-    else if (grayscaleColor.checked) {
-        cellunit.style.backgroundColor = 'gray';
     }
 }
 
-makeRows(16,16);
+function changeColor() {
+    const blackColor = document.getElementById('black');
+    const rainbowColor = document.getElementById('rainbow');
+    const grayscaleColor = document.getElementById('grayscale');
+    const gridItems = document.querySelectorAll('.grid-item');
 
-
-
-
+    gridItems.forEach((item) => {
+        item.count = 0;
+        item.addEventListener('mouseenter', (e) => {    
+            if (blackColor.checked) {
+                e.target.style.backgroundColor = 'black';
+            }
+            else if (rainbowColor.checked) {
+                let color1 = Math.floor(Math.random() * 255)
+                let color2 = Math.floor(Math.random() * 255)
+                let color3 = Math.floor(Math.random() * 255)
+                e.target.style.backgroundColor = 'rgb(' +color1+ ', ' +color2+ ', ' +color3+ ')';
+            }    
+            else if (grayscaleColor.checked) {
+                e.target.style.backgroundColor = 'lightgray';
+                e.target.style.opacity =  1-  0.2 * e.target.count;
+                e.target.count += 1;
+                console.log(e.target.style.opacity)
+            }
+        })
+    })
+}
+    
+function resetBoard() {
+    drawingCanvas.innerHTML = '';
+    makeRows(16,16);
+}
 
 function handleChange1() {
     drawingCanvas.innerHTML = '';
-    makeRows(60,60)  
+    makeRows(60,60) 
+    changeColor() 
 }
 
 function handleChange2() {
     drawingCanvas.innerHTML = '';
     makeRows(35,35)
+    changeColor()
 }
-
 
 function handleChange3() {
     drawingCanvas.innerHTML = '';
     makeRows(16,16)
+    changeColor()
 }
+
+makeRows(16,16);
+changeColor();
