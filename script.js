8836f6954d1e722e465100a6d895fa6ddc723838
techwithjenn <93089897+techwithjenn@users.noreply.github.com>
2021-11-13 19:16:41 -0500

    Set up the sketching function

--- script.js
+++ script.js
@@ -1,12 +1,102 @@
-const drawingCanvas = document.getElementById('canvas');
+var drawingCanvas = document.getElementById('canvas');
+
+var dimensions = 16;
+
+function changeColor (target) {
+    target.style.backgroundColor = 'black'
+}
 
 
-const dimensions = 16;
 
-for (i=0; i < dimensions; i++) {
-    for (j = 0; j < dimensions; j++) {
-        const canvasUnit = document.createElement('div');
-        canvasUnit.classList.add('unit');
-        canvasUnit.textContent = ' '
-        drawingCanvas.append(canvasUnit);
-}}
+// container.addEventListener("mouseover", function (e) {
+//     target = e.target;
+
+//     if (target.matches("div.cell")) {
+//         changeColor(target);
+//     }
+// });
+
+
+function drawBoard() {
+    for (i=0; i < dimensions; i++) {
+        for (j = 0; j < dimensions; j++) {
+            var canvasUnit = document.createElement('div');
+            canvasUnit.classList.add('unit');
+            drawingCanvas.append(canvasUnit);
+            
+
+        }
+    }       
+}
+
+drawBoard();
+
+
+drawingCanvas.addEventListener('mouseover', function(e) {
+    target = e.target;
+    if (target.matches('div.unit')) {
+        changeColor(target);
+    }
+})
+
+
+// for (i=0; i < dimensions; i++) {
+//     for (j = 0; j < dimensions; j++) {
+//         var canvasUnit = drawingCanvas.createElement('div');
+//         canvasUnit.classList.add('unit');
+//         drawingCanvas.append(canvasUnit);
+//         canvasUnit.addEventListener('mouseover', Etch());
+            
+        // var gridUnits = drawingCanvas.querySelectorAll('unit');
+        // gridUnits.forEach(gridUnit => gridUnit.addEventListener('mouseover', () => Etch(gridUnit)))
+
+//     }
+// }        
+
+
+
+
+// Adjust size of blocks for other grid sizes
+// Fix mouseover issue
+
+
+// document.querySelectorAll('.unit').addEventListener('mouseover', event => {
+//     document.querySelectorAll('.unit').classList.add('drawn');})
+
+
+  
+
+
+
+
+// --------------------
+
+function handleChange1() {
+    drawingCanvas.innerHTML = '';
+    dimensions = 100;    
+    drawBoard(dimensions);
+    console.log('it changed 100')
+
+
+    console.log(drawingCanvas.getElementsByTagName('div').length);
+
+}
+
+function handleChange2() {
+    drawingCanvas.innerHTML = '';
+
+    dimensions = 50;
+
+    drawBoard(dimensions);
+    console.log('it changed 50')
+    console.log(drawingCanvas.getElementsByTagName('div').length);
+
+}
+
+
+function handleChange3() {
+    drawingCanvas.innerHTML = '';
+    dimensions = 16;
+    drawBoard(dimensions);
+    console.log('it changed 16');
+}
